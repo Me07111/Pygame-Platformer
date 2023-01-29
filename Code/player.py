9796ae4f547900e9d17d534919a42ef06fa6f1e7
Me07111 <106484211+Me07111@users.noreply.github.com>
2023-01-29 16:36:45 +0100

    player

--- Code/player.py
+++ Code/player.py
@@ -4,24 +4,14 @@ class Character(pygame.sprite.Sprite):
         super().__init__()
         self.pos = InPos
         self.direction = pygame.math.Vector2(0,0)
-        self.jumpSpeed = 50
+        self.jumpSpeed = 0.01
+        self.jumpIndex = 1
         self.gravity = 500
-        self.speed = 0.0001
+        self.speed = 0.01
         self.color = color
         self.surface = surface
-        self.image = pygame.image.load("../Graphics/Character.png")
+        self.image = pygame.image.load("Graphics\Character.png")
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
         print(self.rect)
-    
-    def update(self,delta):
-        self.move(delta)
-    def move(self,delta):
-        self.direction.x = 0
-        keys = pygame.key.get_pressed()
-        if(keys[pygame.K_a]):
-            self.direction.x = -1
-        if(keys[pygame.K_d]):
-            self.direction.x = 1
-        self.pos += self.direction * self.speed * delta
-        self.rect.center = self.pos
+        
