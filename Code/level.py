44811d260346b57a9d4e0524876cacdc7926cffb
Me07111 <106484211+Me07111@users.noreply.github.com>
2023-01-31 21:29:36 +0100

    started writing the generatePlatforms function

--- Code/level.py
+++ Code/level.py
@@ -1,6 +1,7 @@
 import pygame
 from player import Character
 from config import map
+from platformBase import PlatformBase
 
 class Level:
     def __init__(self,screen,width,height,gravity):
@@ -10,16 +11,20 @@ class Level:
         self.height = height
         self.player1 = Character(pygame.math.Vector2(self.width/2, self.height/2),pygame.Color(255,0,0,255),self.screen,20,50)
         self.gravity = gravity
+        self.platforms = pygame.sprite.Group()
 
     def setup(self):
         self.PlayerGroup.add(self.player1)
+        self.generatePlatforms()
+
     def update(self,delta):
-            keys = pygame.key.get_pressed()
-            self.horizontalUpdate(delta,keys)
-            self.verticalUpdate(delta,keys)
-            self.player1.rect.center = self.player1.pos
-            self.PlayerGroup.draw(self.screen)
-            #print(self.player1.rect.center,keys[pygame.K_SPACE])
+        keys = pygame.key.get_pressed()
+        self.horizontalUpdate(delta,keys)
+        self.verticalUpdate(delta,keys)
+        self.player1.rect.center = self.player1.pos
+        self.PlayerGroup.draw(self.screen)
+        self.platforms.draw(self.screen)
+
     def horizontalUpdate(self,delta,keys):
         self.player1.direction.x = 0
         if(keys[pygame.K_a]):
@@ -29,10 +34,24 @@ class Level:
         self.player1.pos.x += self.player1.direction.x * self.player1.speed * delta
 
     def verticalUpdate(self,delta,keys):
-            if(keys[pygame.K_SPACE]):
-                if(self.player1.jumpIndex > 0):
-                    self.player1.direction.y -= self.player1.jumpSpeed
-                    self.player1.jumpIndex -= 1
-            self.player1.direction.y += (self.player1.direction.y + self.gravity) * delta
-            self.player1.pos.y += self.player1.direction.y    
-            print("delta = ",delta)
+        if(keys[pygame.K_SPACE]):
+            if(self.player1.jumpIndex > 0):
+                self.player1.direction.y -= self.player1.jumpSpeed
+                self.player1.jumpIndex -= 1
+        self.player1.direction.y += (self.player1.direction.y + self.gravity) * delta
+        self.player1.pos.y += self.player1.direction.y
+
+    def generatePlatforms(self):
+        cellHeight = self.height/len(map)
+        cellWidth = self.width/len(map[0])
+        for i, row in map:
+            for j, cell in row:
+                if(cell == "o"):
+                    continue
+                elif(cell == "x"):
+                    pos = pygame.math.Vector2(self.width/j,self.height/i)
+                    platform = PlatformBase(pos,cellWidth,cellHeight,pygame.color.Color(255,255,255,255))
+                    self.platforms.add(platform)
+                     
+
+
--- Code/platformBase.py
+++ Code/platformBase.py
@@ -1,5 +1,8 @@
 import pygame
-class platformBase(pygame.sprite.Sprite):
+class PlatformBase(pygame.sprite.Sprite):
     def __init__(self,InPos,width,height,color):
         self.image = pygame.image.load()
         self.color = color
+        self.image = pygame.transform.scale(pygame.image.load("..Graphics/Character.png").fill(color),(width,height))
+        self.rect = self.image.get_rect()
+        self.rect.center = InPos
