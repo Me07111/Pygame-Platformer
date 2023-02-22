9f79241a830c93837cebbaf548b8782594031978
Me07111 <106484211+Me07111@users.noreply.github.com>
2023-02-23 00:46:49 +0100

    made main menu good

--- Code/button.py
+++ Code/button.py
@@ -1,7 +1,7 @@
 import pygame
 from config import renderText
 class Button():
-    def __init__(self,pos,size,text,color = pygame.Color(255,255,255),textSize = 20,textColor = pygame.Color(0,0,0),hoveredColor = pygame.Color(0,0,0,100),pressedColor = pygame.Color(0,0,0)):
+    def __init__(self,pos,size,text,color = pygame.Color(255,255,255),textSize = 20,textColor = pygame.Color(0,0,0),hoveredColor = pygame.Color(0,0,0,50),pressedColor = pygame.Color(0,0,0)):
         super().__init__()
         self.rect = pygame.Rect(pos,size)
         self.rect.center = pos
@@ -20,6 +20,7 @@ class Button():
                 return True
             else:
                 pygame.draw.rect(screen,self.hoveredColor,self.rect)
-        pygame.draw.rect(screen,self.color,self.rect)
+        else:
+            pygame.draw.rect(screen,self.color,self.rect)
         renderText(screen,self.text,"timesnewroman",self.textSize,self.textColor,(self.rect.centerx - 20,self.rect.centery))
         return False
--- Code/mainMenu.py
+++ Code/mainMenu.py
@@ -3,8 +3,8 @@ from level import Level
 from button import Button
 class MainMenu():
     def __init__(self,screen,width,height,clock):
-        self.quit = Button((200,200),(200,50),"Quit")
-        self.play = Button((500,200),(200,50),"Play")
+        self.quit = Button((width/2-200-50,height/2-50),(200,50),"Quit",pygame.Color(255,0,0),20,pygame.Color(0,0,0))
+        self.play = Button((width/2 + 250,height/2-50),(200,50),"Play",pygame.Color(0,255,0),20,pygame.Color(0,0,0))
         self.screen = screen
         self.width = width
         self.height = height
@@ -12,6 +12,7 @@ class MainMenu():
     
     def update(self,delta,gametime,levelHandler):
         if(self.quit.update(self.screen)):
-            pygame.quit()
+            #pygame.quit()
+            pass
         if(self.play.update(self.screen)):
             levelHandler.setLevel(Level(self.screen,self.width,self.height,25,self.clock,2,0))
