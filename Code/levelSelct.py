0984bc4f5d49a777e590bf96aefc9bfb183f2ada
Me07111 <106484211+Me07111@users.noreply.github.com>
2023-03-05 22:13:17 +0100

    made levelselect scale

--- Code/levelSelct.py
+++ Code/levelSelct.py
@@ -1,16 +1,18 @@
 from level import Level
 from button import Button
+from config import scaleValue,scaleRect
 class LevelSelect:
     def __init__(self,screen,width,height,clock,playerCount):
         self.screen = screen
         self.levels = [
             Level(screen,width,height,25,clock,playerCount,0),
             Level(screen,width,height,25,clock,playerCount,1),
-            Level(screen,width,height,25,clock,playerCount,2)
+            Level(screen,width,height,25,clock,playerCount,2),
         ]
         self.buttons = []
+        row = 1
         for i in range(len(self.levels)):
-            button = Button((100 + 270*i,200),(200,50),f"Level {i}")
+            button = Button((scaleValue(height,100) + scaleValue(height,270)*i,scaleValue(height,200)),scaleRect(height,(200,50)),f"Level {i+1}")
             self.buttons.append(button)
 
     def update(self,delta,gametime,levelHandler):
