fe74b4691308640cc4bf8d8c21d914232289f4cc
Me07111 <106484211+Me07111@users.noreply.github.com>
2023-03-05 22:38:33 +0100

    made levelselect levels stack in grid

--- Code/levelSelct.py
+++ Code/levelSelct.py
@@ -2,6 +2,7 @@ from level import Level
 from button import Button
 from config import scaleValue,scaleRect
 import pygame
+import math
 class LevelSelect:
     def __init__(self,screen,width,height,clock,playerCount):
         self.screen = screen
@@ -9,12 +10,16 @@ class LevelSelect:
             Level(screen,width,height,25,clock,playerCount,0),
             Level(screen,width,height,25,clock,playerCount,1),
             Level(screen,width,height,25,clock,playerCount,2),
+            Level(screen,width,height,25,clock,playerCount,0),
+            Level(screen,width,height,25,clock,playerCount,1),
+            Level(screen,width,height,25,clock,playerCount,2)
         ]
         self.buttons = []
-        row = 1
-        for i in range(len(self.levels)):
-            button = Button((scaleValue(height,100) + scaleValue(height,270)*i,scaleValue(height,200)),scaleRect(height,(200,50)),f"Level {i+1}",pygame.Color(0,0,0),scaleValue(height,20))
-            self.buttons.append(button)
+        for i in range(math.ceil(len(self.levels) / 4)):
+            for j in range(4):
+                if(len(self.levels) - 1 >= i*4 + j):
+                    button = Button(scaleRect(height,(200 + 270*j,100 + i * 200)),scaleRect(height,(200,50)),f"Level {i*4 + j +1}",pygame.Color(255,255,255),scaleValue(height,20))
+                    self.buttons.append(button)
 
     def update(self,delta,gametime,levelHandler):
         for i, button in enumerate(self.buttons):
